774860187c1920538d354f46ce9d86e08c5eb822
Lukasz Macniak <lpmacniak@poczta.onet.pl>
2020-07-06 19:45:48 +0200

    dodano modyfikacje funkcji mil_do_km

--- R/mil_do_km2.R
+++ R/mil_do_km2.R
@@ -1,3 +1,20 @@
+#' Konwersja mile na kilometry. metoda alternatywna
+#'
+#'
+#' @description Funkcja sluzacaca do przeliczania podanej
+#' odleglosci w milach na odleglosci w kilometrach
+#'
+#' @param  par zawieracjacy wartosci numeryczne (mile)
+#'
+#' @return wektor num
+#' @export
+#'
+#' @examples
+#' mil_do_km(230)
+#' mil_do_km(34.22)
+#' mil_do_km(c(44,66,88))
+
+
 mil_do_km2=function(par){
   km=par/0.62137
   km
